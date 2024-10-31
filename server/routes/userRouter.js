4b8499b40dd92a74eb40f5ce7f89179569fa1fb0
JuanCarlosAlmarioBabilonia <almariobabilonia@gmail.com>
2024-10-31 07:13:12 -0500

    feat: :sparkles: fixredirect

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -41,7 +41,7 @@ router.get('/auth/discord', passport.authenticate('discord', { scope: ['identify
  */
 router.get('/auth/discord/callback', passport.authenticate('discord', {
     successRedirect: 'https://juancarlosalmariobabilonia.github.io/Exposicion/src/view/dashBoard', // Página después de iniciar sesión
-    failureRedirect: 'https://exposicion-ashy.vercel.app/' // Página en caso de fallo en la autenticación
+    failureRedirect: 'https://exposicion-six.vercel.app/' // Página en caso de fallo en la autenticación
 }));
 
 /**
